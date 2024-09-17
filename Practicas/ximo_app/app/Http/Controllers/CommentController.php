b8e29a64431ec73afe93659a07e9b4102a415c9d
mingarret <ximomingarro@hotmail.com>
2024-09-17 21:09:54 +0200

    Subida de cambios en ximo_app

---
+++ Practicas/ximo_app/app/Http/Controllers/CommentController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class CommentController extends Controller
+{
+    public $comentarios;
+
+    // Constructor para inicializar comentarios desde la sesión
+    public function __construct()
+    {
+        // Carga los comentarios de la sesión o establece un array vacío si no existen
+        $this->comentarios = session('comentarios', ["1 comentario", "2 comentario"]);
+    }
+
+    // Muestra los comentarios desde la sesión
+    public function index()
+    {
+    $html = "<h1>Comentarios</h1><ul>";
+    foreach ($this->comentarios as $index => $comentario) {
+        $html .= "<li><a href='/comments/{$index}'>{$comentario}</a></li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+    }
+
+
+    // Formulario para crear comentarios
+    function create()
+    {
+    return "
+        <form method='POST' action='/comments'>
+            <input type='hidden' name='_token' value='" . csrf_token() . "'>
+            <input type='text' name='comment'>
+            <input type='submit' value='Enviar'>
+        </form>";
+    }
+
+    // Almacena el comentario en la sesión
+    public function store(Request $request)
+        {
+        // Agregar el comentario
+        array_push($this->comentarios, $request->input('comment'));
+
+        // Guardar los comentarios en la sesión
+        session(['comentarios' => $this->comentarios]);
+
+        // Redirigir al método index para mostrar los comentarios en HTML
+        return redirect()->action([CommentController::class, 'index']);
+        }
+
+
+    // Buscar por id  
+    function show(string $commentid)
+        {
+            
+            return $this->comentarios[$commentid];     
+        }
+    //Editar un comentario
+    function edit(int $commentid)
+        {
+            // Simplemente devuelve un formulario HTML para editar el comentario
+            return "
+                <form method='POST' action='/comments/{$commentid}' enctype='multipart/form-data'>
+                    <input type='hidden' name='_method' value='PATCH'>
+                    <input type='text' name='comment' value='{$this->comentarios[$commentid]}'>
+                    <input type='submit' value='Actualizar'>
+                </form>
+            ";
+        }
+    //Para que muestre la actualizacion
+    function update(Request $request, int $commentid)
+        {
+            // Actualizar el comentario en el array temporal
+            $this->comentarios[$commentid] = $request->input('comment');
+
+            // Devolver el array actualizado de comentarios
+            return $this->comentarios;
+        }
+    //Para que elimine un comentario
+    public function destroy(int $commentid)
+        {
+        // Eliminar el comentario en la posición dada
+        unset($this->comentarios[$commentid]);
+
+        // Re-indexar el array para mantener los índices consecutivos
+        $this->comentarios = array_values($this->comentarios);
+
+        // Actualizar los comentarios en la sesión
+        session(['comentarios' => $this->comentarios]);
+
+        // Devolver el array actualizado de comentarios
+        return redirect()->action([CommentController::class, 'index']);
+        }
+
+}
---
+++ Practicas/ximo_app/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\CommentController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get( '/comments', [CommentController::class, 'index']);
+Route::get('/comments/create', [CommentController::class, 'create']);
+Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/comments/{commentid}', [CommentController::class, 'show']);
+Route::get('/comments/{commentid}/edit', [CommentController::class, 'edit']);
+Route::patch('/comments/{commentid}', [CommentController::class, 'update']);
+Route::delete('/comments/{commentid}', [CommentController::class, 'destroy']);
+
